c607dabf1ea5faa66fb9f8a95e09104b321970b6
YugaEgo <yet@ego.team>
2022-04-21 22:53:22 +0300

    Add variables/

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+}
