dab4ebed04dd7bd25364e80299b7724a42e80f07
YugaEgo <yet@ego.team>
2022-10-04 19:07:17 +0300

    Accept argument to enable case insensitive search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,7 +17,11 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let ignore_case = if args.len() > 3 {
+            true
+        } else {
+            env::var("IGNORE_CASE").is_ok()
+        };
 
         Ok(Config { query, file_path, ignore_case, })
     }
