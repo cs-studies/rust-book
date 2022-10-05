60ba0b3ce51bf7541cf96d24fea1e1302ab285d0
YugaEgo <yet@ego.team>
2022-10-05 21:09:16 +0300

    Add 'rectangles'

---
+++ reclangles/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code)]
+
+fn main() {
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+
