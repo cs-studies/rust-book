d7569dc7306815673f24006fa5ef304d20ed9516
YugaEgo <yet@ego.team>
2022-10-23 20:05:31 +0300

    Add 'minigrep' with chapter 13 updates

---
+++ minigrep13/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+
+use minigrep13::Config;
+
+fn main() {
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep13::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
